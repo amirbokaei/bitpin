f85b714486639276f7519d442baca1773a1f7f21
Amir Bokaei <amir.h.bokaei@gmail.com>
2022-10-23 15:37:12 +0330

    startapp User and set auth user model to app

--- bitpin1/settings.py
+++ bitpin1/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Content.apps.ContentConfig',
+    'User.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
+
