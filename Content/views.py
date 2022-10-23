695ab2be052448eac8c0043e52178477692ab7a5
Amir Bokaei <amir.h.bokaei@gmail.com>
2022-10-23 18:15:59 +0330

    add RateCreateView and RateUpdateView

--- Content/views.py
+++ Content/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView
+from rest_framework.response import Response
+from .models import Rate
+from .models import Content
+from .serializers import ContentSerializer, RateSerializer
+from rest_framework import permissions
+
 
 # Create your views here.
