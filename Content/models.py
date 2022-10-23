96b6313b1c8e769e31fc3af645d220579cc84cf7
Amir Bokaei <amir.h.bokaei@gmail.com>
2022-10-23 18:15:07 +0330

    add count_rate_users and rate_avg

--- Content/models.py
+++ Content/models.py
@@ -5,3 +5,13 @@ class Content(models.Model):
     title = models.CharField(max_length=256)
     text = models.TextField()
 
+    @property
+    def count_rate_users(self):
+        return Rate.objects.filter(content=self).aggregate(Count('id'))
+
+    @property
+    def rate_avg(self):
+        return Rate.objects.filter(content=self).aggregate(Avg('score'))
+
+
+
